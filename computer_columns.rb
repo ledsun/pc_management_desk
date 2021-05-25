1f79ae41c20702e926b341b3e54d741809be6e7b
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-05-25 10:52:27 +0900

    feature: Read environment variables if you are using the dotenv-rails gem

--- computer_columns.rb
+++ computer_columns.rb
@@ -2,6 +2,13 @@ require 'erb'
 require 'yaml'
 require 'active_record'
 
+# dotenv-rails gemを使っていたら環境変数を読み込む
+begin
+  require 'dotenv/load'
+rescue LoadError
+  # 何もしない
+end
+
 ActiveRecord::Base.establish_connection YAML.load(ERB.new(File.read('config/database.yml')).result)['development']
 
 model_name = ARGV[0]
