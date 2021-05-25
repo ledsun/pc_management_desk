2a02d02894143e3165a925ce2228e478705f1518
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-05-25 10:35:54 +0900

    feature: Class names can be specified by arguments

--- computer_columns.rb
+++ computer_columns.rb
@@ -3,7 +3,9 @@ require 'yaml'
 require 'active_record'
 
 ActiveRecord::Base.establish_connection YAML.load(ERB.new(File.read('config/database.yml')).result)['development']
-class Computer < ActiveRecord::Base; end
-Computer.columns.each do |c|
+
+model_name = ARGV[0]
+class_name= Object.const_set model_name, Class.new(ActiveRecord::Base)
+class_name.columns.each do |c|
   printf "%20s %10s \n", c.name, c.type
 end
